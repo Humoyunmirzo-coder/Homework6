7e6c5cd18073ed46a56b3dbe67c924518c1c1c9d
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2023-06-06 19:50:01 +0500

    a

---
+++ Homework6/FullnameFile.cs
@@ -0,0 +1,15 @@
+﻿
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+         string file = "C:\\Gitignore\\test.txt";
+
+        var filesExe = Directory.GetFiles(file, "*exe*", SearchOption.AllDirectories);
+        foreach (var fileX in filesExe)
+        {
+            Console.WriteLine(Path.GetFileNameWithoutExtension(fileX));
+        }
+    }
+}
