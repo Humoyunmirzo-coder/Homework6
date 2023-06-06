4c7750693ae4fa1f0a29eb1ef691b4666a259cdd
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2023-06-06 20:17:53 +0500

    a

---
+++ Homework6/Bin_Obj_Clear.cs
@@ -0,0 +1,15 @@
+﻿
+static void Main(string[] args)
+{
+    
+    string path = "C:\\Textnew";
+    string[] Clear = Directory.GetDirectories(path, "bin* "); foreach (string bin in Clear)
+    { 
+        Console.WriteLine(bin + " DELETED"); Directory.Delete(bin, true); 
+    }
+    string[] ClearObj = Directory.GetDirectories(path, "obj*.");
+    foreach (string obj in ClearObj)
+    {
+        Console.WriteLine(obj + " DELETED"); Directory.Delete(obj, true);
+       };
+}   
